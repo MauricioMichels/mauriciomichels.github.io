9aecb30457fd03a5798b32bb16d68f3f6fc6ef28
Gabriel Andrade Michalski <Gabriel Andrade Michalski>
2016-10-25 20:05:02 -0200

    gabriel

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-﻿var actualWord = null
+﻿var actualWord = null;
+var actualPlayer = {};
 
-
-var speak = function (word) {
+var speak = function(word) {
 	var speech = new SpeechSynthesisUtterance();
 
 	speech.text = word;
@@ -13,18 +13,64 @@ var speak = function (word) {
 	speechSynthesis.speak(speech);
 }
 
-var getNotUsedWord = function () {
-	
+var getNotUsedWord = function() {
 	var index = Math.floor(Math.random() * (words.length + 1));
 	return words[index];
 }
-actualWord = getNotUsedWord();
-console.log(actualWord);
 
-var speakActualWord = function () {
+var removeUsedWord = function(word) {
+	words.splice(word, 1);
+}
+
+var speakActualWord = function() {
 	speak(actualWord.word);
 }
 
-var speakActualDefinition = function () {
+var speakActualDefinition = function() {
 	speak(actualWord.definition);
 }
+
+var initGame = function() {
+	var name = document.querySelector('#name').value;
+	if (name) {
+		setName(name);
+		gameMode();
+		actualWord = getNotUsedWord();
+
+	} else {
+		alert('Informe um nome de usuario');
+	}
+}
+
+var setName = function(name) {
+	actualPlayer.name = name;
+	document.querySelector('#nameLabel').innerHTML = actualPlayer.name;
+}
+var verifyWord = function() {
+	var word = document.querySelector('#word').value;
+	if (word.toUpperCase() == actualWord.word.toUpperCase()) {
+		alert('acertou carai');
+		nextWord();
+		var word = document.querySelector('#word').value = "";
+	} else {
+		confirm('Errou');
+		window.location.reload()
+	}
+}
+
+var nextWord = function() {
+	removeUsedWord(actualWord);
+	actualWord = getNotUsedWord();
+}
+
+var initMode = function() {
+	document.querySelector('#init').style.display = 'block';
+	document.querySelector('#game').style.display = 'none';
+}
+
+var gameMode = function() {
+	document.querySelector('#game').style.display = 'block';
+	document.querySelector('#init').style.display = 'none';
+}
+
+initMode();
