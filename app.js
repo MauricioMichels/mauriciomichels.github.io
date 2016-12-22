63f6b6a94ea7605227132779e86aedf21ae26822
Mauricio Michels <mauricio@kugel.com.br>
2016-12-22 18:41:00 -0200

    init

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var total = 0;
 
 
 
-var calcular = function(argument) {
+var calcular = function() {
 
 	cal22 = document.querySelector('#cal22').value * 0.80;
 	document.querySelector('#resultCal22').innerHTML = "R$: " + cal22;
@@ -24,6 +24,30 @@ var calcular = function(argument) {
 	document.querySelector('#resultCal12').innerHTML = "R$: " + cal12;
 
 	total = cal22 + cal380 + cal40 + cal357 + calRT + cal12;
+	final = total + 25;
 
-	document.querySelector('#total').innerHTML = "25.00 + " + total + " = " +(total+25);
+	document.querySelector('#total').innerHTML = "25.00 + " + total + " = ";
+	document.querySelector('#final').innerHTML = final;
+	final
+};
+
+
+var limpar = function() {
+
+	document.querySelector('#cal22').value  = null;
+	document.querySelector('#cal380').value = null;
+	document.querySelector('#cal357').value = null;
+	document.querySelector('#calRT').value  = null;
+	document.querySelector('#cal12').value  = null;
+
+	document.querySelector('#resultCal22').innerHTML = 	"";
+	document.querySelector('#resultCal380').innerHTML = "";
+	document.querySelector('#resultCal40').innerHTML = 	"";
+	document.querySelector('#resultCal357').innerHTML = "";
+	document.querySelector('#resultCalRT').innerHTML = 	"";
+	document.querySelector('#resultCal12').innerHTML = 	"";
+
+	document.querySelector('#total').innerHTML = "";
+	document.querySelector('#final').innerHTML = "";
+
 }
